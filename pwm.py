a262b295a67f7c888ee50d790fb27834f1c528f9
Gavin Zhang <gavinz0228@gmail.com>
2015-05-14 22:27:17 -0400

    fixed esc memorization problem

---
+++ pwm.py
@@ -0,0 +1,27 @@
+from RPIO import PWM
+import RPi.GPIO as GPIO
+import time
+#GPIO.setmode(GPIO.BCM)
+pin=17
+max=2000
+min=1000
+servo=PWM.Servo()
+def calibrate():
+	
+	servo.set_servo(pin,max)
+	key=raw_input("waiting")
+	print(key)
+	servo.set_servo(pin,min)
+	#time.sleep(2)
+	key=raw_input("waiting")
+	
+def run():
+	servo.set_servo(pin,min)
+	time.sleep(2)
+	while True:
+		servo.set_servo(pin,1500)
+		print('yay')
+		time.sleep(1)
+if __name__=="__main__":
+	#calibrate()
+	run()
--- qc.py
+++ qc.py
@@ -1,12 +1,31 @@
-import vw
+from RPIO import PWM
+from mpu import *
+import RPi.GPIO as GPIO
 import time
-import pigpio
-RX=2
-BPS=4000
-pi=pigpio.pi()
-rx=vw.rx(pi,RX,BPS)
-tx=vw.tx(pi,TX,BPS)
-while True:
-	while not rx.ready():
-		time.sleep(1)
-	print(rx.get())
+#GPIO.setmode(GPIO.BCM)
+pin=17
+max=2000
+min=1000
+motor=PWM.Servo()
+baseSpeed=1200
+def run():
+	init()
+	while True:
+		addSpeed=getX()*10
+		speed=baseSpeed+addSpeed
+		setSpeed(pin,speed)
+		time.sleep(0.1)
+def init():
+	setSpeed(pin,min)
+	time.sleep(2)
+def setSpeed(pin,speed):
+	speed=int(speed)
+	if speed>max:
+		speed=max
+	elif speed<min:
+		speed=min
+	speed=speed/10*10
+	motor.set_servo(pin,int(speed))	
+if __name__=="__main__":
+	#calibrate()
+	run()
